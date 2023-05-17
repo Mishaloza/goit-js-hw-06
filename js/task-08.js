2dd7fc9c7e4395a5a5d7fb225cad5e4be4fcde68
Mishaloza <mishaloza@gmail.com>
2023-05-17 13:28:35 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginFormEl = document.querySelector(".login-form");
+const submitBtn = document.querySelector(".login-form button[type=submit]");
 
+loginFormEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const emailValue = event.currentTarget.elements.email.value;
+  const passwordValue = event.currentTarget.elements.password.value;
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+    const user = {
+      email: emailValue,
+      password: passwordValue,
+    };
+    console.log(user);
+    event.currentTarget.reset();
+  }
+}
